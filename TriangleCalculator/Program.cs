a6ded5127ef9ba94571c7e1ce08203943ef311e3
ecmillan-dev <85765807+ecmillan-dev@users.noreply.github.com>
2021-06-14 12:04:27 -0400

    Add project files.

---
+++ TriangleCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriangleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
